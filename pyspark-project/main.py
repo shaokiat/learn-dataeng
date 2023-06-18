ab46c93047c4043240e3bd26421996bcfb352733
shaokiat <shaokiat@gmail.com>
2023-06-18 23:15:01 +0800

    add data

--- pyspark-project/main.py
+++ pyspark-project/main.py
@@ -1,3 +1,21 @@
 import findspark
 from pyspark import SparkContext
+from pyspark.sql import SparkSession, Window, Row
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import matplotlib.pyplot as plt
 
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+
+def load_dataframe(filename):
+    df = spark.read.format('csv').options(header='true').load(filename)
+    return df
+
+
+df_matches = load_dataframe('./Data/Matches.csv')
+df_matches.limit(5).show()
